c8642383d3178e0936692e61f9004ea557fdb65d
Sachin Kumar <157154133+SKSingh0703@users.noreply.github.com>
2024-10-12 17:27:57 +0530

    Update OpenAccount.jsx

--- frontend/src/landing_page/OpenAccount.jsx
+++ frontend/src/landing_page/OpenAccount.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import { useNavigate } from "react-router-dom";
 
 function OpenAccount() {
+   const navigate = useNavigate();
   return (
     <div className='container p-5 text-center mb-5' >
        <div className="row">
@@ -10,7 +12,9 @@ function OpenAccount() {
         <p className=' opacity-75 mt-4 mb-4'>
         Modern platforms and apps, ₹0 investments, and flat ₹20 intraday and F&O trades.
         </p>
-        <button className='opacity-75 p-2 mt-3 btn btn-primary fs-5 mb-5' style={{width:"20%",margin:"0 auto"}}>Sign up for free</button>
+        <button onClick={() => {
+            navigate("/signup")
+        }} className='opacity-75 p-2 mt-3 btn btn-primary fs-5 mb-5' style={{width:"20%",margin:"0 auto"}}>Sign up for free</button>
        </div>
        
     </div>
