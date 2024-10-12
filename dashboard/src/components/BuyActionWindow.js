68aeab2a8d137f3d54fe304b2f214ae30208eaaf
Sachin Kumar <157154133+SKSingh0703@users.noreply.github.com>
2024-10-12 16:53:02 +0530

    Update BuyActionWindow.js

--- dashboard/src/components/BuyActionWindow.js
+++ dashboard/src/components/BuyActionWindow.js
@@ -13,7 +13,7 @@ const BuyActionWindow = ({ uid }) => {
   const { closeBuyWindow } = useContext(GeneralContext);
 
   const handleBuyClick = () => {
-    axios.post("http://localhost:3002/newOrder", {
+    axios.post("https://zerodha-clone-backend-dmzr.onrender.com/newOrder", {
       name: uid,
       qty: stockQuantity,
       price: stockPrice,
